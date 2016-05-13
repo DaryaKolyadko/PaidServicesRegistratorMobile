a2559db8ca3641d560d3dffcfdb8288140426fdd
Minchuk Sergei <minchuk94@gmail.com>
2016-05-13 20:57:33 +0200

    design activity_login | activity_main | activity_service_list| activity_settings

--- PaidServiceRegistrator/app/src/main/java/com/darya/paidserviceregistrator/MainActivity.java
+++ PaidServiceRegistrator/app/src/main/java/com/darya/paidserviceregistrator/MainActivity.java
@@ -1,13 +1,71 @@
 package com.darya.paidserviceregistrator;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton buttonService;
+    private ImageButton buttonSettings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupActionBar();
+        setUpComponents();
+        setButtonsListeners();
+    }
+
+    private void setButtonsListeners() {
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ServiceListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setUpComponents() {
+        buttonService = (ImageButton) findViewById(R.id.imgBtnServices);
+        buttonSettings = (ImageButton) findViewById(R.id.imgBtnSettings);
+    }
+
+    private void setSupActionBar() {
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- PaidServiceRegistrator/app/src/main/java/com/darya/paidserviceregistrator/SettingsActivity.java
+++ PaidServiceRegistrator/app/src/main/java/com/darya/paidserviceregistrator/SettingsActivity.java
@@ -2,12 +2,28 @@ package com.darya.paidserviceregistrator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class SettingsActivity extends AppCompatActivity {
+    private NumberPicker numberPickerStringLength;
+    private int MIN_STRING_LENGTH = 15;
+    private int MAX_STRING_LENGTH = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setComponents();
+        setNumberPickerStringLength(MIN_STRING_LENGTH, MAX_STRING_LENGTH);
+    }
+
+    private void setComponents() {
+        numberPickerStringLength = (NumberPicker) findViewById(R.id.numberPickerStringLength);
+    }
+
+    private void setNumberPickerStringLength(int min, int max) {
+        numberPickerStringLength.setMinValue(min);
+        numberPickerStringLength.setMaxValue(max);
+        numberPickerStringLength.setValue(min);
     }
 }
